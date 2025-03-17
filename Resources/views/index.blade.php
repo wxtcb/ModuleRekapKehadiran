b004b90b3f065ae30ba0995c0af9c02471a30178
ernakha <ernakhasanah78@gmail.com>
2025-03-17 09:49:51 +0700

    first commit

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('rekapkehadiran::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('rekapkehadiran.name') !!}
+    </p>
+@endsection
